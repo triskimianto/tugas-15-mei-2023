0bd6e4121ea36796d415fe4437b75a0a57c3e567
Tito Riskimianto <118734924+triskimianto@users.noreply.github.com>
2023-05-15 14:23:12 +0700

    second commit

---
+++ layouts/header.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="bg-gradient-to-r from-violet-400 to-pink-400 py-4">
+        <div class="mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex items-center justify-between h-16">
+                <div class="flex items-center">
+                    <div class="flex-shrink-0">
+                        <span class="text-2xl text-white font-bold"><slot/></span>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
